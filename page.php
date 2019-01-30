32f135499d0530decfc1d204f9826137436fb225
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2019-01-30 18:19:06 +0100

    Serious Music

--- page.php
+++ page.php
@@ -11,15 +11,6 @@ get_header(); ?>
 				<div class="col-md-8">
 				</div>
 				<div class="col-md-4">
-					<div class="aanvragen">						
-						<div class="close-aanvragen">
-							<div></div>
-							<div></div>
-						</div>					
-						<h2><?php the_field( 'titel_aanvragen', 'option' ); ?></h2>						
-						<?php $formidable = get_field( 'formidable_shortcode', 'option' ); ?>
-						<?php echo do_shortcode($formidable); ?>
-					</div>
 				</div>
 				<h1 class="title">
 					<?php echo the_title();?>
@@ -29,15 +20,10 @@ get_header(); ?>
 	</div>
 <?php } ?>
 
-<section id="content">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 marginauto">
-	<?php echo the_content();?>
-			</div>
-		</div>
-	</div>
-</section>
+
+<?php get_template_part( 'template-parts/flexible', 'content' ); ?>
+
+
 <section id="andere-diensten">
 	<div class="container">
 		<div class="row">
